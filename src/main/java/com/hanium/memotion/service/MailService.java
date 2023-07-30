fd570cc7ce7b03665f88612805558dabb97c4cbe
aeeazip <codnjs0809@naver.com>
2023-07-30 21:38:11 +0900

    feat(#2) : Dockerfile 작성

--- src/main/java/com/hanium/memotion/service/MailService.java
+++ src/main/java/com/hanium/memotion/service/MailService.java
@@ -37,7 +37,8 @@ public class MailService {
     @Value("${spring.mail.username}")
     private String email;
 
-    private MimeMessage createMessage(String code, String emailTo) throws MessagingException, UnsupportedEncodingException {
+    // 비밀번호 변경 이메일 내용
+    private MimeMessage createPasswordMessage(String code, String emailTo) throws MessagingException, UnsupportedEncodingException {
         MimeMessage message = javaMailSender.createMimeMessage();
         message.addRecipients(MimeMessage.RecipientType.TO, emailTo); //보내는 사람
         message.setSubject("Memotion 이메일로 임시 비밀번호 찾기 :"); //메일 제목
@@ -64,8 +65,8 @@ public class MailService {
         return generatedString + "!";
     }
 
-    // 메일 발송
-    public String sendMail(String email) throws BaseException {
+    // 메일로 임시 비밀번호 발송
+    public String sendPasswordMail(String email) throws BaseException {
         //이메일 중복 확인
         Optional<Member> getMember = memberRepository.findByEmail(email);
         if(getMember.isEmpty())
@@ -79,10 +80,51 @@ public class MailService {
             memberRepository.save(member);
 
             // 2. 임시 비밀번호 메일로 보내주기
-            MimeMessage mimeMessage = createMessage(password, email);
-            log.info("ok");
+            MimeMessage mimeMessage = createPasswordMessage(password, email);
             javaMailSender.send(mimeMessage);
             return password;
+        } catch (Exception e){
+            e.printStackTrace();
+            throw new BaseException(ErrorCode.EMAIL_SEND_ERROR);
+        }
+    }
+
+    // 인증 코드 이메일 내용
+    private MimeMessage createCodeMessage(String code, String emailTo) throws MessagingException, UnsupportedEncodingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.addRecipients(MimeMessage.RecipientType.TO, emailTo); //보내는 사람
+        message.setSubject("Memotion 이메일 인증번호 :"); //메일 제목
+        message.setText(code, "utf-8", "html"); //내용, charset타입, subtype
+        message.setFrom(new InternetAddress(email,"Memotion_Official")); //보내는 사람의 메일 주소, 보내는 사람 이름
+
+        log.info("message : " + message);
+        return message;
+    }
+
+    // 인증 코드 생성 (5자리로 생성)
+    private String createCode() {
+        StringBuffer code = new StringBuffer();
+        Random random = new Random();
+
+        for(int i=0; i<5; i++) {
+            code.append((random.nextInt(10)));
+        }
+
+        return code.toString();
+    }
+
+    // 메일로 인증번호 발송
+    public String sendCodeMail(String email) throws BaseException {
+        //이메일 중복 확인
+        Optional<Member> getMember = memberRepository.findByEmail(email);
+        if(getMember.isEmpty())
+            throw new BadRequestException("해당하는 회원을 찾을 수 없습니다.");
+
+        String code = createCode();
+        try{
+            MimeMessage mimeMessage = createPasswordMessage(code, email);
+            javaMailSender.send(mimeMessage);
+            return code;
         } catch (Exception e){
             e.printStackTrace();
             throw new BaseException(ErrorCode.EMAIL_SEND_ERROR);
