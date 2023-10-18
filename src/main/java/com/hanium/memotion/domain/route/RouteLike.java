033946084510ca30f1b0f589308edac3280d472d
aeeazip <codnjs0809@naver.com>
2023-10-18 16:17:50 +0900

    feat : route domain 추가 #10

---
+++ src/main/java/com/hanium/memotion/domain/route/RouteLike.java
@@ -0,0 +1,30 @@
+package com.hanium.memotion.domain.route;
+
+import com.hanium.memotion.domain.core.BaseTime;
+import com.hanium.memotion.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "RouteLike")
+public class RouteLike extends BaseTime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long routeLikeId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "route_id")
+    private Route route;
+}
